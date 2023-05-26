859cc638ca5194bc86decb3bf036375f8965e403
Emmanuel <emmanuelutofa@gmail.com>
2023-05-26 16:57:42 +0200

    Made changes to the code to remove bugs

---
+++ modules/config.js
@@ -0,0 +1,3 @@
+// config.js
+export const API_BASE_URL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api';
+export const API_KEY = 'ByGdzSo2L0OI2OKu'
---
+++ src/index.js
@@ -0,0 +1,108 @@
+/* eslint-disable prefer-destructuring */
+// main.js
+import './styles.css';
+import EventEmitter from '../modules/event-class.js';
+import fetchScores from '../modules/fetch-scores.js';
+import submitScore from '../modules/submit-score.js';
+import { API_BASE_URL, API_KEY } from '../modules/config.js';
+
+const eventEmitter = new EventEmitter();
+
+document.addEventListener('DOMContentLoaded', async () => {
+  const gameName = 'puuzzlex';
+  let gameId = '';
+
+  const createNewGame = async (name) => {
+    try {
+      const response = await fetch(`${API_BASE_URL}/games/${API_KEY}/scores/`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name,
+        }),
+      });
+      const data = await response.json();
+      gameId = data.result.split(' ')[3];
+    } catch (error) {
+      eventEmitter.emit('fetchError', error);
+    }
+  };
+
+  await createNewGame(gameName);
+
+  const refreshScores = async () => {
+    const scoresList = document.querySelector('.list-group');
+    scoresList.innerHTML = '';
+
+    try {
+      const scores = await fetchScores(gameId);
+      scores.reverse().forEach((score, index) => {
+        const listItem = document.createElement('li');
+        listItem.classList.add('list-group-item', 'border-dark');
+        listItem.style.border = '1px solid black';
+        listItem.style.listStyleType = 'none';
+        listItem.style.padding = '10px';
+        listItem.style.width = '90%';
+
+        if (index % 2 === 1) {
+          listItem.style.background = 'dimgray';
+        }
+
+        listItem.textContent = `${score.user}: ${score.score}`;
+        scoresList.insertBefore(listItem, scoresList.firstChild); // Add the new item at the top of the list
+      });
+    } catch (error) {
+      eventEmitter.emit('fetchError', error);
+    }
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    const nameInput = document.querySelector('input#name');
+    const scoreInput = document.querySelector('input#score');
+
+    const userName = nameInput.value.trim();
+    const score = parseInt(scoreInput.value.trim(), 10);
+
+    if (userName !== '' && !Number.isNaN(score)) {
+      try {
+        await submitScore(gameId, userName, score);
+        nameInput.value = '';
+        scoreInput.value = '';
+        refreshScores();
+      } catch (error) {
+        eventEmitter.emit('fetchError', error);
+      }
+    }
+  };
+
+  const form = document.querySelector('form');
+  form.addEventListener('submit', handleFormSubmit);
+
+  const preventListDisplay = () => {
+    const scoresList = document.querySelector('.list-group');
+    scoresList.style.display = 'none'; // Hide the scores list initially
+
+    const refreshButton = document.querySelector('.refresh');
+    refreshButton.addEventListener('click', () => {
+      scoresList.style.display = 'block'; // Show the scores list when the refresh button is clicked
+    });
+
+    const submitButton = document.querySelector('.submit');
+    submitButton.addEventListener('click', () => {
+      scoresList.style.display = 'block'; // Show the scores list when the submit button is clicked
+    });
+  };
+
+  preventListDisplay(); // Call the preventListDisplay function to prevent initial list display
+
+  const refreshButton = document.querySelector('.refresh');
+  refreshButton.addEventListener('click', refreshScores);
+
+  refreshScores();
+});
+
+
