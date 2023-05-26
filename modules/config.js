78d0c95ad8882671b10d04305911a6f189410c4c
Emmanuel <emmanuelutofa@gmail.com>
2023-05-26 10:29:51 +0200

    Revising all errors deleted files

--- modules/config.js
+++
@@ -1,3 +0,0 @@
-// config.js
-const API_BASE_URL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api';
-export default API_BASE_URL;
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,17 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: {
-    index: './src/index.js',
-  },
+ module.exports = {
+   entry: {
+     index: './src/index.js',     
+   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Leaderboard',
-      template: './src/index.html',
+        template: './src/index.html',
     }),
-  ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-};
+  ],  
+   output: {
+     filename: '[name].bundle.js',
+     path: path.resolve(__dirname, 'dist'),
+   },
+ };
